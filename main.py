61eef1e8071286968a90fb3fbde1c0aecbfb4018
Danya Generalov <danya@danya02.ru>
2019-10-17 15:07:28 +0300

    Adding basic creation and tracking.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from peewee import *
+import uuid
 
 import base64
 import datetime
+import json
 
 db = SqliteDatabase('tracking-pixels.db')
 db.connect()
@@ -33,20 +35,31 @@ PNG_PIXEL = base64.b64decode(b'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC
 
 @app.route('/<address>')
 def serve_pixel(address):
-    # TODO: add tracking here
+    try:
+        pixel = Pixel.get(Pixel.address==address)
+    except DoesNotExist:
+        return 'pixel id '+address+' doesnt exist', 404
+    if Visit.select().where(Visit.pixel == pixel).count()==0:
+        pass # TODO: do something interesting if it's the first visit on this pixel
+    visit = Visit(pixel=pixel, user_agent=request.headers.get('User-Agent', None), ip_address=request.remote_addr, additional_params=json.dumps(request.args))
+    visit.save()
     response = make_response(PNG_PIXEL)
     response.headers.set('Content-Type', 'image/png')
     return response
 
-@app.route('/create')
+@app.route('/create', methods=['GET','POST'])
 def create():
-    return 'Creation: To be implemented'
+    uid = str(uuid.uuid4())
+    p = Pixel(name='Test pixel '+uid, address=uid, description='Test pixel at addr '+uid, access_password=uid)
+    p.save()
+
+    return 'Your pixel address is: '+uid
 
 @app.route('/stats/<address>')
 def stats(address):
     return 'Statistics: To be implemented'
 
-@app.route('/delete/<address>')
+@app.route('/delete/<address>', methods=['GET','POST'])
 def delete(address):
     return 'Deletion: To be implemented'
 
